db69956988c1e03fab9a2ebff512c6b89d0e624b
jonoman55 <jonoman55@yahoo.com>
2021-07-17 11:36:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const fetchStates = async () => {
+    const res = await axios.get('states.json');
+    return res.data;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [states, setStates] = useState([]);
+    const [filteredStates, setFilteredStates] = useState([]);
+    const [input, setInput] = useState('');
+
+    const getStates = async () => {
+        try {
+            setStates(await fetchStates());
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getStates();
+    }, [input]);
+
+    const searchStates = input => {
+        setInput(input);
+        if (input) {
+            setFilteredStates(states.filter(state => {
+                const regex = new RegExp(`^${input}`, 'gi');
+                return state.name.match(regex) || state.abbr.match(regex);
+            }));
+        } else {
+            setFilteredStates([]);
+        }
+    };
+
+    return (
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-md-6 m-auto'>
+                    <h3 className='text-center mb-3' style={{ color: '#fff', fontSize: 38 }}>
+                        <i className='fas fa-flag-usa'></i> State Capital Lookup
+                    </h3>
+                    <div className='form-group mb-1'>
+                        <input
+                            type='text'
+                            id='search'
+                            className='form-control form-control-lg'
+                            placeholder='Enter state name or abbreviation...'
+                            autoComplete='off'
+                            onChange={(e) => searchStates(e.target.value)} />
+                    </div>
+                    <div>
+                        {filteredStates.length > 0 && filteredStates.map((match, idx) =>
+                            <div className='card card-body mb-1' key={idx}>
+                                <h4>{match.name} ({match.abbr}) <span className='text-primary'>{match.capital}</span></h4>
+                                <small>Lat: {match.lat} / Long {match.long}</small>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
